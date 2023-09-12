5f65531eced0f98a09440c218c7bcfed9d0c893c
Omar Ouhra <omarmaroc26@gmail.com>
2023-09-12 16:01:04 +0100

    fix: listing ad video config for wide resolution

--- src/templates/listingAd.ts
+++ src/templates/listingAd.ts
@@ -2,30 +2,30 @@ export const listingAdConfig = (data: any) => {
   return {
     template: `
     <body class="bg-white">
-        <div class="flex flex-col items-center justify-center h-screen">
+        <div class="flex flex-col items-center justify-center h-screen py-12">
             <div class="relative flex items-center justify-center w-[90%] h-auto">
               <img src=${data?.images[6]}
                   alt="Image 1"
-                  class="w-[90%]  h-auto opacity-0 animate-images absolute rounded-xl"
+                  class="w-full lg:w-[800px] object-contain h-auto opacity-0 animate-images absolute rounded-xl "
                   style="animation-delay: 0s;">
               <img src=${data?.images[7]}
                           alt="Image 2"
-                  class="w-[90%]  h-auto opacity-0 animate-images absolute rounded-xl"
+                  class="w-full lg:w-[800px] object-contain h-auto opacity-0 animate-images absolute rounded-xl"
                           style="animation-delay: 2s;">
                <img src=${data?.images[8]}
                   alt="Image 2"
-                  class="w-[90%]  h-auto opacity-0 animate-images absolute rounded-xl"
+                  class="w-full lg:w-[800px] object-contain h-auto opacity-0 animate-images absolute rounded-xl"
                   style="animation-delay: 4s;">
                <img src=${data?.images[9]}
                   alt="Image 2"
-                  class="w-[90%]  h-auto opacity-0 animate-images absolute rounded-xl"
+                  class="w-full lg:w-[800px] object-contain h-auto opacity-0 animate-images absolute rounded-xl"
                   style="animation-delay: 6s;">
             </div>
 
 
                <p class="text-5xl text-center opacity-0 animate-textAnimation" style="animation-delay: 8s;">Welcome to
               </p>
-               <p class="text-5xl text-center opacity-0 animate-textAnimation mt-6 font-semibold" style="animation-delay: 8s;"> ${data?.title}
+               <p class="text-5xl text-[#FF5A5F] text-center opacity-0 animate-textAnimation mt-6 font-semibold" style="animation-delay: 8s;"> ${data?.title}
               </p>
 
               <div class='animate-textAnimation opacity-0 absolute bg-white h-screen w-full flex items-center justify-center'
